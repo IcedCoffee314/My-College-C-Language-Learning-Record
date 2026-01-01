fcadbfecd95927b6961f7cd241b8902fa4c32389
IcedCoffee314 <1491441606@qq.com>
2026-01-01 22:37:05 +0800

    Add files via upload

---
+++ class 10.20/main.c
@@ -0,0 +1,96 @@
+//#include <stdio.h>
+//int main()
+//{
+//    int num;
+//    scanf("%d",&num);
+//    printf("%d", num%2!=0);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//int i = 0,a=0,b=2,c =3,d=4;
+////i = a++ && ++b && d++;
+//i = a++||++b||d++;
+//printf("a = %d\nb = %d\nc = %d\nd = %d\n", a, b, c, d);
+//return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    char ch;
+//    ch = getchar();
+//    putchar(++ch);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    char ch;
+//    ch = getchar();
+//    printf("Hello%cWorld%c", ch, ch);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    double a;
+//    scanf("%lf", &a);
+//    printf("%f\n", a);
+//    printf("%e\n", a);
+//    printf("%.2f\n", a);
+//    printf("%10.3f\n", a);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int a, b;
+//    scanf("%*1d%3d%*1d%2d", &a, &b);
+//    printf("%d+%d=%d", a, b, a + b);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int a, b, c, d, e;
+//    scanf("%2d%2d%2d%2d%2d", &a, &b, &c, &d, &e);
+//    printf("sum=%5d\n", a + b + c + d + e);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int a, b;
+//    scanf("%x %x", &a, &b);
+//    printf("%X", a + b);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int a, b, c, d;
+//    scanf("%d %d %d %d", &a, &b, &c, &d);
+//    printf("Sum = %d; ", a + b + c + d);
+//    printf("Average = %.1f", (a + b + c + d) / 4.0);
+//    return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    a = 4;
+    b = 7;
+    c = a + b;
+    printf ("c = %d\n", c);
+    return 0;
+}
