844210bbc06db6b5cfbe734a0aca85cfcfb580da
IcedCoffee314 <1491441606@qq.com>
2026-01-01 22:24:21 +0800

    Add files via upload

---
+++ class 9.29/main.c
@@ -0,0 +1,55 @@
+
+//#include <stdio.h>
+//int main()
+//{
+//    float r;
+//    scanf("%f", &r);
+//    printf("area=%.2f", r * r * 3.14159);
+//    return 0;
+//}
+
+//    #
+//   # #
+//  #   #
+// #     #
+//#       #
+// #     #
+//  #   #
+//   # #
+//    #
+
+//#include <stdio.h>
+//int main()
+//{
+//	int fahr = 0, celsius;
+//	fahr = 0;  /* 温度表的下限 */
+//	while (fahr <= 300)  /* 循环 */
+//	{
+//		celsius = 5 * (fahr-32) / 9;
+//		printf("%d\t%d\n", fahr, celsius);  /* 输出 */
+//		fahr = fahr + 20;  /* 加上间距 */
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    printf("  0 -17\n");
+//    printf(" 20  -6\n");
+//    printf(" 40   4\n");
+//    printf(" 60  15\n");
+//    printf(" 80  26\n");
+//    printf("100  37\n");
+//    printf("120  48\n");
+//    printf("140  60\n");
+//    printf("160  71\n");
+//    printf("180  82\n");
+//    printf("200  93\n");
+//    printf("220 104\n");
+//    printf("240 115\n");
+//    printf("260 126\n");
+//    printf("280 137\n");
+//    printf("300 148\n");
+//    return 0;
+//}
