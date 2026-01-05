1f831666e0187fd61baf9f4b74f61db013c370c7
IcedCoffee314 <1491441606@qq.com>
2026-01-05 18:39:23 +0800

    Update main.c

--- class 12_29/main.c
+++ class 12_29/main.c
@@ -1,4 +1,4 @@
-//����ж������һ���ַ��Ƿ�Ϊ�����ġ�����ν�����ġ�����ָ˳���͵�����һ�����ַ������硰level���͡�ABCCBA�����ǻ��ġ�
+//编程判断输入的一串字符是否为“回文”。所谓“回文”，是指顺读和倒读都一样的字符串，如“level”和“ABCCBA”都是回文。
 //#include <stdio.h>
 //int main()
 //{
@@ -15,16 +15,16 @@
 //        }
 //    }
 //    if(k)
-//        printf("\"%s\"�ǻ���", a);
+//        printf("\"%s\"是回文", a);
 //    else
-//        printf("\"%s\"���ǻ���", a);
+//        printf("\"%s\"不是回文", a);
 //    return 0;
 //}
 
-//��һ���ַ�����Ѱ���Ƿ������һ���ַ�����������ڣ������Ӵ����ַ�����λ�ã���������ڣ��򷵻�-1��
+//在一个字符串中寻找是否存在另一个字符串，如果存在，返回子串在字符串的位置，如果不存在，则返回-1；
 //#include <stdio.h>
 //#include <string.h>
-//// �Ƚ������ַ����Ƿ���ȣ���ȷ���1�����ȷ���0
+//// 比较两个字符串是否相等，相等返回1，不等返回0
 //int cmp_str(const char *s1, const char *s2)
 //{
 //    int i;
@@ -37,7 +37,7 @@
 //        return 1;
 //    return 0;
 //}
-//// ˼·��a�ַ�����0��ʼ��ȥ��b�ַ����Ƚϣ�������Ⱦ������ƶ�����һ���ַ��������Ƚϣ���Ⱦͱ�ʾ�ҵ���
+//// 思路：a字符串从0开始，去与b字符串比较，如果不等就往后移动到下一个字符，继续比较，相等就表示找到了
 //int find_str(const char *a, const char *b)
 //{
 //    int i, j;
@@ -73,7 +73,7 @@
 //    return 0;
 //}
 
-//Ѱ��������Ӵ�
+//寻找最长公共子串
 //#include <stdio.h>
 //#include <string.h>
 //void find_str(const char *a, const char *b)
@@ -120,7 +120,7 @@
 //    return 0;
 //}
 
-//����һ�����£�ͳ�������еĵ�������
+//输入一段文章，统计文章中的单词数。
 //#include <stdio.h>
 //int main()
 //{
@@ -138,7 +138,7 @@
 //    return 0;
 //}
 
-//���Swap����
+//设计Swap函数
 //#include <stdio.h>
 //void Swap(int *x, int *y)
 //{
@@ -156,7 +156,7 @@
 //    return 0;
 //}
 
-//����һ���ṹ������STUDENT������ѧ�ţ����������䣬�������Ա��ֵ���������
+//定义一个结构体类型STUDENT，包含学号，姓名，年龄，输入各成员的值，再输出。
 //#include <stdio.h>
 //struct STUDENT
 //{
@@ -176,7 +176,7 @@
 //    return 0;
 //}
 
-//���������ṹ�������ֵ
+//交换两个结构体变量的值
 //#include <stdio.h>
 //struct TIME
 //{
@@ -211,9 +211,9 @@
 //    return 0;
 //}
 
-//�о����������Էֳ��Ժ͸��ԣ�ÿ���о�����¼�俼�ź����������Կ����Σ�100����Ӣ�100������ѧ��150����רҵ�Σ�150����
-//���Էֱ��ԣ�80�������ԣ�120�����ܳɼ��ɳ��Գɼ��͸��Գɼ����ɣ����г��Գɼ�ռ50%�����Գɼ�ռ50%��
-//�����һ���ṹ������������������Ϣ��������ÿ�������ĳ��Գɼ������Գɼ����ܳɼ������ܳɼ��Ӵ�С�����
+//研究生招生考试分初试和复试，每个研究生记录其考号和姓名，初试考政治（100）、英语（100）、数学（150）、专业课（150），
+//复试分笔试（80）和面试（120），总成绩由初试成绩和复试成绩构成，其中初试成绩占50%，复试成绩占50%。
+//请设计一个结构体类型来描述上述信息，并计算每个考生的初试成绩、复试成绩和总成绩并按总成绩从大到小输出。
 //#include <stdio.h>
 //struct STUDENT
 //{
@@ -263,7 +263,7 @@
 //        Get(&stu[i]);
 //    }
 //    PX(stu, n);
-//    printf("����--- ����---- ���� Ӣ�� ��ѧ רҵ ���� ���� ���� ���� �ܷ�-\n");
+//    printf("考号--- 姓名---- 政治 英语 数学 专业 笔试 面试 初试 复试 总分-\n");
 //    for(i=0;i<n;i++)
 //    {
 //        printf("%-7s %-8s %4d %4d %4d %4d %4d %4d %4d %4d %5.1f\n", stu[i].id, stu[i].name,
@@ -273,10 +273,10 @@
 //    return 0;
 //}
 
-//���˵Ǽǿ�������дһ�����򣬽��ǼǵĲ��˰�������ԭ���ų��������Ⱥ�˳��
-//1. �����ˣ������60�꣩�ȷ����������ȿ�����
-//2. �����˰�����Ӵ�С��˳�򿴲���������ͬ�İ��Ǽǵ��Ⱥ�˳������
-//3. �������˰��Ǽǵ��Ⱥ�˳�򿴲���
+//病人登记看病，编写一个程序，将登记的病人按照以下原则排出看病的先后顺序：
+//1. 老年人（年龄≥60岁）比非老年人优先看病。
+//2. 老年人按年龄从大到小的顺序看病，年龄相同的按登记的先后顺序排序。
+//3. 非老年人按登记的先后顺序看病。
 //#include <stdio.h>
 //struct OLD
 //{
@@ -335,3 +335,116 @@
 //    }
 //    return 0;
 //}
+
+//[C语言][第八章]根据姓名查找记录
+//这是一个输入姓名查找某学生信息的程序，请补充完整
+
+//当不加双引号输入学号时，是一个数字，应该用长整型存储
+//#include <stdio.h>
+//#include <string.h>
+//#define N 8
+//struct DATA
+//{
+//    long long num;   //学号用长整型存储
+//    char name[10];
+//    int a1;
+//    int a2;
+//};
+//struct DATA *func(struct DATA *p, int n, char name[])
+//{
+//    int i;
+//    for(i=0;i<n;i++)
+//    {
+//        if(strcmp(p->name ,name)==0)
+//        {
+//            return p;
+//        }
+//        else
+//            p++;
+//    }
+//    return NULL;
+//}
+//void output(struct DATA *p)
+//{
+//    if(p==NULL)
+//    {
+//        printf("Not find!");
+//    }
+//    else
+//    {
+//        printf("%lld,%s,%d,%d", p->num, p->name, p->a1, p->a2);
+//    }
+//}
+//int main()
+//{
+//    struct DATA data[N] =
+//    {
+//        {2023141450008, "zhao", 81, 93},  //学号数字对应long long类型
+//        {2023141450007, "qian", 83, 83},
+//        {2023141450006, "sun", 81, 74},
+//        {2023141450005, "li", 81, 74},
+//        {2023141450004, "zhou", 98, 87},
+//        {2023141450003, "wu", 82, 70},
+//        {2023141450002, "zheng", 73, 82},
+//        {2023141450001, "wang", 95, 92}
+//    };
+//    char name[10];
+//    scanf("%s", name);
+//    struct DATA *p = func(data, N, name);
+//    output(p);
+//    return 0;
+//}
+
+//当加双引号输入学号时，才是一个字符串，用字符数组存储
+#include <stdio.h>
+#include <string.h>
+#define N 8
+struct DATA
+{
+    char num[20];    // 学号用字符串存储
+    char name[10];
+    int a1;
+    int a2;
+};
+struct DATA *func(struct DATA *p, int n, char name[])
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(strcmp(p->name, name) == 0)
+        {
+            return p;
+        }
+        p++;
+    }
+    return NULL;
+}
+void output(struct DATA *p)
+{
+    if(p == NULL)
+    {
+        printf("Not find!");
+    } else
+    {
+        printf("%s,%s,%d,%d", p->num, p->name, p->a1, p->a2);
+    }
+}
+int main()
+{
+    struct DATA data[N] =
+    {
+        {"2023141450008", "zhao", 81, 93},   //学号字符串对应char类型数组
+        {"2023141450007", "qian", 83, 83},
+        {"2023141450006", "sun", 81, 74},
+        {"2023141450005", "li", 81, 74},
+        {"2023141450004", "zhou", 98, 87},
+        {"2023141450003", "wu", 82, 70},
+        {"2023141450002", "zheng", 73, 82},
+        {"2023141450001", "wang", 95, 92}
+    };
+    char name[10];
+    scanf("%s", name);
+    struct DATA *p = func(data, N, name);
+    output(p);
+    return 0;
+}
